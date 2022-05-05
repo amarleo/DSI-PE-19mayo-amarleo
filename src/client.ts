b5091e68baa6f11670e9630593e5f2005a31a205
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-05-05 10:03:55 +0100

    first version

---
+++ src/client.ts
@@ -0,0 +1,23 @@
+import * as net from 'net';
+
+export type commandType = {
+  command: string,
+  args?: string[],
+}
+
+const command: string = process.argv[2];
+const args: string[] = process.argv.splice(3, process.argv.length);
+const wholeCommand: commandType = {
+  command: command,
+  args: args,
+};
+
+const client = net.connect({port: 60300});
+
+client.write(JSON.stringify(wholeCommand));
+
+client.on('data', (dataJSON) => {
+  const message = dataJSON.toString();
+  console.log(message);
+  client.end();
+});
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,32 @@
-export function index(): string {
-  return 'index';
-}
+import * as net from 'net';
+import {spawn} from 'child_process';
+
+net.createServer({allowHalfOpen: true}, (connection) => {
+  console.log('A client has connected.');
+
+  connection.on('close', () => {
+    console.log('A client has disconnected');
+  });
+
+  connection.on('data', (data) => {
+    const JSONdata = JSON.parse(data.toString());
+
+    const clientCommand = spawn(JSONdata.command, JSONdata.args);
+
+    if (JSONdata.args.length === 0) {
+      console.log(`Command ${JSONdata.command} executed`);
+    } else {
+      console.log(
+          `Command ${JSONdata.command}` +
+          ` with arguments ${JSONdata.args} exectuted`);
+    }
+    let clientCommandOutput: string = '';
+    clientCommand.stdout.on('data', (piece) => clientCommandOutput += piece);
+
+    clientCommand.on('close', () => {
+      connection.write(clientCommandOutput);
+    });
+  });
+}).listen(60300, () => {
+  console.log('Waiting for clients to connect');
+});
